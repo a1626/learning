2112ba71bbb75814539595d3d4274abe04241675
a1626 <gupta.abhishek1804@gmail.com>
2017-03-04 14:08:52 +0530

    adding multiple new commits

---
+++ js/for loop.js
@@ -0,0 +1,13 @@
+//types of for loop. All three are optional in for loop
+
+
+//no initial, incemental or conditional exp (conditional is true by defaul) 
+for(;;) {
+    console.log("yes i work");
+}
+
+
+//no initial, incemental or conditional exp (conditional is true by defaul) 
+for(;true;) {
+    console.log("yes i work");
+}
